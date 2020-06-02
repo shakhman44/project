36a647c8f60dc6974f5eada5353ec4704969993c
shakhman <sadzhiev@ya.ru>
2020-06-02 23:21:24 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//alert("Hello!");
+//const result = confirm("Are you here?");
+//console.log(result);
+//const answer = prompt("Вам есть 18?", "18");
+//console.log(typeof(answer));
+//const answers = [];
+//answers[0] = prompt("Как ваше имя?", "");
+//answers[1] = prompt("Как ваша фамилия?", ""); 
+//answers[2] = prompt("Сколько вам лет?", "");
+
+//document.write(answers);
+//console.log(typeof(answers));
+//alert(`Привет, ${answers[0]} ${answers[1]}!, Вам ${answers[2]} года!`);
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
