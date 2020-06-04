dedfeb4a0c8b910778b04698b93a0f2b36dd3302
shakhman <sadzhiev@ya.ru>
2020-06-04 23:10:03 +0300

    задание по уроку 12

--- js/script.js
+++ js/script.js
@@ -13,9 +13,28 @@
 //document.write(answers);
 //console.log(typeof(answers));
 //alert(`Привет, ${answers[0]} ${answers[1]}!, Вам ${answers[2]} года!`);
-let incr = 10,
-    decr = 10;
-incr++;
-decr--;
-console.log(incr);
-console.log(decr);
+//let incr = 10,
+//    decr = 10;
+//incr++;
+//decr--;
+//console.log(incr);
+//console.log(decr);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMoiveDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из полследних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцениваете его?', ''),
+      c = prompt('Один из полследних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцениваете его?', '');
+
+personalMoiveDB.movies[a] = b;
+personalMoiveDB.movies[c] = d;
+
+console.log(personalMoiveDB);
